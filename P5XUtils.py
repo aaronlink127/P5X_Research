b1e3669127a56f38c538847176c6b991639d3a9c
aaronlink127 <32995197+aaronlink127@users.noreply.github.com>
2025-10-22 19:35:17 -0400

    Add vFileContent reading, add extracting capabilities

--- P5XUtils.py
+++ P5XUtils.py
@@ -2,40 +2,161 @@ import sys
 import os
 import io
 import struct
+from io import StringIO, BytesIO
 
 def getAssetHashCode(assetName: str):
     n = 0
     for i in assetName:
         n = ord(i) + 131*n
     return n & 0x7FFFFFFF
-    
-def buildBundleMap(gameDir: str):
-    d = {}
-    clientDir = os.path.join(gameDir,"client")
-    outerPackageBundles = os.path.join(clientDir, "OuterPackage", "Bundles", "Windows")
-    if os.path.isdir(outerPackageBundles):
-        for numDir in os.listdir(outerPackageBundles):
-            numPath = os.path.join(outerPackageBundles, numDir)
-            if os.path.isdir(numPath):
-                for bundleName in os.listdir(numPath):
-                    d[bundleName] = os.path.join(numPath, bundleName)
-    else:
-        print("WARN: OuterPackage not found!")
-    fileContOut = os.path.join(clientDir, "bin", "fileContentOut")
-    if os.path.isdir(fileContOut):
-        fileContBundles = os.path.join(fileContOut, "Bundles", "Windows")
-        for numDir in os.listdir(fileContBundles):
-            numPath = os.path.join(fileContBundles, numDir)
-            if os.path.isdir(numPath):
-                for bundleName in os.listdir(numPath):
-                    if bundleName not in d:
-                        d[bundleName] = os.path.join(numPath, bundleName)
-    else:
-        print("WARN: fileContentOut not found! It's recommended to run P5X_vFileContentExtract first!")
-    return d
+def getBundleObfusOffset(name):
+    return (getAssetHashCode(name) & 0x1F) + 8
+
+def readInt32(f):
+    return struct.unpack("<i", f.read(4))[0]
 
 def readUInt32(f):
     return struct.unpack("<I", f.read(4))[0]
+
+def openBytes(s, mode):
+    if "b" in mode:
+        return BytesIO(s)
+    else:
+        return StringIO(s.decode('cp1252'))
+
+class BaseFile:
+    def readAllBytes(self):
+        with self.open("rb") as f:
+            return f.read()
+
+class RealFile(BaseFile):
+    def __init__(self, path):
+        self.path = path
+    def open(self, mode):
+        baseName = os.path.basename(self.path)
+        root, ext = os.path.splitext(baseName)
+        if ext == ".bundle":
+            with open(self.path, "rb") as f:
+                f.seek(getBundleObfusOffset(baseName), io.SEEK_SET)
+                return openBytes(f.read(), mode)
+        else:
+            return io.open(self.path, mode)
+
+class VFile(BaseFile):
+    def __init__(self, srcfs, idx, size, pos):
+        self.srcfs = srcfs
+        self.idx = idx
+        self.size = size
+        self.pos = pos
+    
+    def readAllBytes(self):
+        vContentPath = os.path.join(self.srcfs.root, "_vfileContent" + str(self.idx))
+        with io.open(vContentPath, "rb") as f:
+            f.seek(self.pos, io.SEEK_SET)
+            return f.read(self.size)
+
+    def open(self, mode):
+        allBytes = self.readAllBytes()
+        openBytes(allBytes, mode)
+
+class RealFSMount:
+    def __init__(self, root):
+        self.root = root
+    def getFile(self, path):
+        realpath = os.path.join(self.root, path)
+        if os.path.exists(realpath):
+            return RealFile(realpath)
+
+class VFSMount:
+    def __init__(self, root):
+        self.root = root
+        self.map = {}
+        indexFb = os.path.join(root, "_vfileIndexV2.fb")
+        with io.open(indexFb, "rb") as f:
+            f.seek(f.tell() + readUInt32(f), io.SEEK_SET)
+            f.read(4)
+            f.seek(f.tell() + readUInt32(f), io.SEEK_SET)
+            numContentChunks = readUInt32(f)
+            chunkDataPos = []
+            for i in range(numContentChunks):
+                chunkDataPos.append(f.tell() + readInt32(f))
+            for i in range(numContentChunks):
+                offset = chunkDataPos[i]
+                f.seek(offset, io.SEEK_SET)
+                f.read(4)
+                sectionEnd = f.tell() + readInt32(f)
+                f.seek(f.tell() + readInt32(f), io.SEEK_SET)
+                numEntries = readUInt32(f)
+                entries = []
+                for x in range(numEntries):
+                    entries.append(f.tell() + readUInt32(f))
+                for entryPos in entries:
+                    f.seek(entryPos, io.SEEK_SET)
+                    # print(hex(entryPos))
+                    f.read(4)
+                    entryMetaEnd = f.tell() + readUInt32(f)
+                    fileSize = readUInt32(f)
+                    fileOffset = readUInt32(f)
+                    f.seek(entryMetaEnd, io.SEEK_SET)
+                    filePathLen = readUInt32(f)
+                    filePath = f.read(filePathLen).decode("utf-8")
+                    baseName = os.path.basename(filePath)
+                    root, ext = os.path.splitext(baseName)
+                    if ext == ".bundle":
+                        fileOffset = fileOffset + getBundleObfusOffset(baseName)
+                    self.map[filePath] = VFile(self, i, fileSize, fileOffset)
+
+    def getFile(self, path):
+        if path in self.map:
+            return self.map[path]
+
+class VFileSystem:
+    def __init__(self, gameDir):
+        self.mounts = []
+        self.mounts.append(RealFSMount(os.path.join(gameDir, "client", "OuterPackage")))
+        self.mounts.append(RealFSMount(os.path.join(gameDir, "client", "bin")))
+        self.mounts.append(VFSMount(os.path.join(gameDir, "client", "bin")))
+        # self.mounts.append(RealFSMount(os.path.join(gameDir, "client", "bin", "fileContentOut")))
+
+    def exists(self, path):
+        fp = self.getFile(path)
+        return fp is not None
+
+    def open(self, path, mode):
+        f = self.getFile(path)
+        if f is not None:
+            return f.open(mode)
+
+    def readAllBytes(self, path):
+        f = self.getFile(path)
+        if f is not None:
+            return f.readAllBytes()
+
+    def getFile(self, path):
+        for m in self.mounts:
+            g = m.getFile(path)
+            if g is not None:
+                return g
+    
+def getBundlePath(abName):
+    return "Bundles/Windows/" + str(getAssetHashCode(abName) % 200).zfill(3) + "/" + abName
+
+def printUsage():
+    print("Usage: python " + sys.argv[0] + " <path-to-game> <mode> [mode-args...]")
+    print("Modes:")
+    print("  find-asset <asset-name> - Prints bundle that contains asset")
+    print("  find-asset-dep <asset-name> - Prints bundle that contains asset, and any dependencies")
+    print("  find-bundle-dep <bundle-name> - Prints dependencies of a given bundle")
+    print("  extract-bundle <bundle-name> <output-dir> - Extracts bundle to directory")
+    print("  extract-bundle-dep <bundle-name> <output-dir> - Extracts bundle dependency tree to directory")
+    print("  extract-asset-dep <asset-name> <output-dir> - Extracts asset dependency tree of asset")
+
+if len(sys.argv) <= 2:
+    print("Not enough args")
+    printUsage()
+    exit()
+
+vfs = VFileSystem(sys.argv[1])
 
 class ZeusAssetBundle:
     def __init__(self, name, deps):
@@ -48,29 +169,29 @@ class ZeusAssetManifest:
         self.bundleMap = {}
 
     def loadFromFB(self, filePath):
-        f = io.open(filePath, "rb")
-        f.seek(0x10, io.SEEK_SET)
-        sLen = readUInt32(f)
-        f.seek(sLen - 4, io.SEEK_CUR)
-        assetCount = readUInt32(f)
-        assetOffsets = []
-        for i in range(assetCount):
-            assetOffsets.append(f.tell() + readUInt32(f))
-        for assetOffset in assetOffsets:
-            f.seek(assetOffset, io.SEEK_SET)
-            f.read(4)
-            afterData = f.tell() + readUInt32(f)
-            f.read(8)
-            dependencies = []
-            dependencyCount = readUInt32(f)
-            for y in range(dependencyCount):
-                dependencies.append(readUInt32(f))
-            f.seek(afterData, io.SEEK_SET)
-            bundleNameLen = readUInt32(f)
-            bundleName = f.read(bundleNameLen).decode('utf-8')
-            zeusBundle = ZeusAssetBundle(bundleName, dependencies)
-            self.bundles.append(zeusBundle)
-            self.bundleMap[bundleName] = zeusBundle
+        with vfs.open(filePath, "rb") as f:
+            f.seek(0x10, io.SEEK_SET)
+            sLen = readUInt32(f)
+            f.seek(sLen - 4, io.SEEK_CUR)
+            assetCount = readUInt32(f)
+            assetOffsets = []
+            for i in range(assetCount):
+                assetOffsets.append(f.tell() + readUInt32(f))
+            for assetOffset in assetOffsets:
+                f.seek(assetOffset, io.SEEK_SET)
+                f.read(4)
+                afterData = f.tell() + readUInt32(f)
+                f.read(8)
+                dependencies = []
+                dependencyCount = readUInt32(f)
+                for y in range(dependencyCount):
+                    dependencies.append(readUInt32(f))
+                f.seek(afterData, io.SEEK_SET)
+                bundleNameLen = readUInt32(f)
+                bundleName = f.read(bundleNameLen).decode('utf-8')
+                zeusBundle = ZeusAssetBundle(bundleName, dependencies)
+                self.bundles.append(zeusBundle)
+                self.bundleMap[bundleName] = zeusBundle
     
     def getManifestFromName(self, bundleName):
         if bundleName in self.bundleMap:
@@ -97,89 +218,109 @@ class AssetMap:
         if assetName in self.assetNameMap:
             return self.assetNameMap[assetName]
     def loadFromFB(self, filePath):
-        f = io.open(filePath, "rb")
-        f.seek(0x10, io.SEEK_SET)
-        sLen = readUInt32(f)
-        f.seek(sLen - 4, io.SEEK_CUR)
-        pathCount = readUInt32(f)
-        assetPathOffsets = []
-        for i in range(pathCount):
-            assetPathOffsets.append(f.tell() + readUInt32(f))
-        hashCount = readUInt32(f)
-        assetHashOffsets = []
-        for i in range(hashCount):
-            assetHashOffsets.append(f.tell() + readUInt32(f))
-        for pathOffset in assetPathOffsets:
-            f.seek(pathOffset, io.SEEK_SET)
-            f.read(4)
-            assetNameOffs = f.tell() + readUInt32(f)
-            f.read(4)
-            bundleNameLen = readUInt32(f)
-            bundleName = f.read(bundleNameLen).decode('utf-8')
-            f.seek(assetNameOffs, io.SEEK_SET)
-            assetNameLen = readUInt32(f)
-            assetName = f.read(assetNameLen).decode('utf-8')
-            self.assetNameMap[assetName] = bundleName
-        for hashOffset in assetHashOffsets:
-            f.seek(hashOffset, io.SEEK_SET)
-            f.read(4)
-            assetNameHash = readUInt32(f)
-            extraDataLen = readUInt32(f)
-            extraData = f.read(extraDataLen - 4)
-            bundleNameLen = readUInt32(f)
-            bundleName = f.read(bundleNameLen).decode('utf-8')
-            self.assetHashMap[assetNameHash] = bundleName
+        with vfs.open(filePath, "rb") as f:
+            f.seek(0x10, io.SEEK_SET)
+            sLen = readUInt32(f)
+            f.seek(sLen - 4, io.SEEK_CUR)
+            pathCount = readUInt32(f)
+            assetPathOffsets = []
+            for i in range(pathCount):
+                assetPathOffsets.append(f.tell() + readUInt32(f))
+            hashCount = readUInt32(f)
+            assetHashOffsets = []
+            for i in range(hashCount):
+                assetHashOffsets.append(f.tell() + readUInt32(f))
+            for pathOffset in assetPathOffsets:
+                f.seek(pathOffset, io.SEEK_SET)
+                f.read(4)
+                assetNameOffs = f.tell() + readUInt32(f)
+                f.read(4)
+                bundleNameLen = readUInt32(f)
+                bundleName = f.read(bundleNameLen).decode('utf-8')
+                f.seek(assetNameOffs, io.SEEK_SET)
+                assetNameLen = readUInt32(f)
+                assetName = f.read(assetNameLen).decode('utf-8')
+                self.assetNameMap[assetName] = bundleName
+            for hashOffset in assetHashOffsets:
+                f.seek(hashOffset, io.SEEK_SET)
+                f.read(4)
+                assetNameHash = readUInt32(f)
+                extraDataLen = readUInt32(f)
+                extraData = f.read(extraDataLen - 4)
+                bundleNameLen = readUInt32(f)
+                bundleName = f.read(bundleNameLen).decode('utf-8')
+                self.assetHashMap[assetNameHash] = bundleName
 
-def printUsage():
-    print("Usage: python " + sys.argv[0] + " <path-to-game> <mode> [mode-args...]")
-    print("Modes:")
-    print("  find-asset <asset-name> - Prints bundle that contains asset")
-    print("  find-asset-dep <asset-name> - Prints bundle that contains asset, and any dependencies")
-    print("  find-bundle-dep <bundle-name> - Prints dependencies of a given bundle")
+zeusManifest = ZeusAssetManifest()
+zeusManifest.loadFromFB(getBundlePath("zeusBundleManifest.txt"))
+assetMap = AssetMap()
+assetMap.loadFromFB(getBundlePath("assetMapName.txt"))
 
-if len(sys.argv) > 2:
-    bundleMap = buildBundleMap(sys.argv[1])
-    assert "zeusBundleManifest.txt" in bundleMap, "bundle manifest couldn't be found"
-    assert "assetMapName.txt" in bundleMap, "asset map couldn't be found"
-    zeusManifest = ZeusAssetManifest()
-    zeusManifest.loadFromFB(bundleMap["zeusBundleManifest.txt"])
-    assetMap = AssetMap()
-    assetMap.loadFromFB(bundleMap["assetMapName.txt"])
+def listBundleDeps(man, depth = 0, depSet = None):
+    if depSet is None:
+        depSet = set()
+    if man.name not in depSet:
+        depSet.add(man.name)
+        print("  " * depth + man.name)
+        for dep in man.deps:
+            listBundleDeps(zeusManifest.bundles[dep], depth + 1, depSet)
 
-    def listBundleDeps(man, depth = 0, depSet = None):
-        if depSet is None:
-            depSet = set()
-        if man.name not in depSet:
-            depSet.add(man.name)
-            print("  " * depth + man.name)
-            for dep in man.deps:
-                listBundleDeps(zeusManifest.bundles[dep], depth + 1, depSet)
-
-    match sys.argv[2]:
-        case "find-asset":
-            assetName = sys.argv[3]
-            bundleName = assetMap.getBundleByAsset(assetName)
-            if bundleName is None:
-                print("Couldn't find asset " + assetName)
-            else:
-                print(bundleName)
-        case "find-bundle-dep":
-            bundleName = sys.argv[3]
-            bundleManifest = zeusManifest.getManifestFromName(bundleName)
-            if bundleManifest is None:
-                print("Couldn't find bundle " + bundleName)
-            else:
-                listBundleDeps(bundleManifest)
-        case "find-asset-dep":
-            assetName = sys.argv[3]
-            bundleName = assetMap.getBundleByAsset(assetName)
-            if bundleName is None:
-                print("Couldn't find asset " + assetName)
-            else:
-                listBundleDeps(zeusManifest.getManifestFromName(bundleName))
-        case _:
-            print("Unknown command: " + sys.argv[2])
-            printUsage()
-else:
-    print("Not enough args")
-    printUsage()
+match sys.argv[2]:
+    case "find-asset":
+        assetName = sys.argv[3]
+        bundleName = assetMap.getBundleByAsset(assetName)
+        if bundleName is None:
+            print("Couldn't find asset " + assetName)
+        else:
+            print(bundleName)
+    case "find-bundle-dep":
+        bundleName = sys.argv[3]
+        bundleManifest = zeusManifest.getManifestFromName(bundleName)
+        if bundleManifest is None:
+            print("Couldn't find bundle " + bundleName)
+        else:
+            listBundleDeps(bundleManifest)
+    case "find-asset-dep":
+        assetName = sys.argv[3]
+        bundleName = assetMap.getBundleByAsset(assetName)
+        if bundleName is None:
+            print("Couldn't find asset " + assetName)
+        else:
+            listBundleDeps(zeusManifest.getManifestFromName(bundleName))
+    case "extract-bundle":
+        bundleName = sys.argv[3]
+        outDir = sys.argv[4]
+        bundleManifest = zeusManifest.getManifestFromName(bundleName)
+        if bundleManifest is None:
+            print("Couldn't find bundle " + bundleName)
+        else:
+            allBytes = vfs.readAllBytes(getBundlePath(bundleName))
+            with open(os.path.join(outDir, bundleName), "wb") as f:
+                f.write(allBytes)
+    case "extract-bundle-dep":
+        bundleName = sys.argv[3]
+        outDir = sys.argv[4]
+        bundleManifest = zeusManifest.getManifestFromName(bundleName)
+        if bundleManifest is None:
+            print("Couldn't find bundle " + bundleName)
+        else:
+            deps = zeusManifest.getDependencies(bundleManifest)
+            for dep in deps:
+                allBytes = vfs.readAllBytes(getBundlePath(dep))
+                with open(os.path.join(outDir, dep), "wb") as f:
+                    f.write(allBytes)
+    case "extract-asset-dep":
+        assetName = sys.argv[3]
+        outDir = sys.argv[4]
+        bundleName = assetMap.getBundleByAsset(assetName)
+        if bundleName is None:
+            print("Couldn't find asset " + assetName)
+        else:
+            deps = zeusManifest.getDependencies(zeusManifest.getManifestFromName(bundleName))
+            for dep in deps:
+                allBytes = vfs.readAllBytes(getBundlePath(dep))
+                with open(os.path.join(outDir, dep), "wb") as f:
+                    f.write(allBytes)
+    case _:
+        print("Unknown command: " + sys.argv[2])
+        printUsage()
